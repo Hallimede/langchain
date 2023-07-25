14b0b882f6295491487263eec74ef86dc24dea1b
Hallimede <vincewu9@gmail.com>
2023-07-25 10:09:42 +0800

    Use playwright to load subpages

--- libs/langchain/langchain/document_loaders/url_playwright.py
+++ libs/langchain/langchain/document_loaders/url_playwright.py
@@ -6,6 +6,8 @@ from typing import List, Optional
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
 
+from urllib.parse import urlparse
+
 logger = logging.getLogger(__name__)
 
 
@@ -125,4 +127,71 @@ class PlaywrightURLLoader(BaseLoader):
                     else:
                         raise e
             await browser.close()
+        return docs
+
+    async def aload_with_subpages(self, n) -> List[Document]:
+        """Load the specified URLs with Playwright and create Documents asynchronously.
+        Use this function when in a jupyter notebook environment.
+
+        Returns:
+            List[Document]: A list of Document instances with loaded content.
+        """
+        from playwright.async_api import async_playwright
+
+        docs: List[Document] = list()
+
+        async with async_playwright() as p:
+            browser = await p.chromium.launch(headless=self.headless)
+            docs = await self.crawl_pages(browser, self.urls, True)
+            await browser.close()
+        return docs
+
+    async def crawl_pages(self, browser, urls, is_root, n) -> List[Document]:
+        from unstructured.partition.html import partition_html
+        docs: List[Document] = list()
+        for url in urls:
+            print(f"Loading {url}")
+            try:
+                page = await browser.new_page()
+                await page.goto(url)
+
+                if is_root:
+
+                    # Extract hyperlinks from the page
+                    links = await page.evaluate('''() => {
+                        return Array.from(document.querySelectorAll('a'))
+                            .map(a => a.href);
+                    }''')
+
+                    # Select 10 hyperlinks under the same domain
+                    url_start = f"{urlparse(url).scheme}://{urlparse(url).netloc}"
+
+                    links_to_be_crawled = set()
+
+                    for link in links:
+                        if link.startswith(url_start) and len(links_to_be_crawled) < n:
+                            links_to_be_crawled.add(link)
+                    
+                    print("Sub links to be crawled:")
+                    print(links_to_be_crawled)
+                    docs.extend(await self.crawl_pages(browser, list(links_to_be_crawled), False))
+
+                for selector in self.remove_selectors or []:
+                    elements = await page.locator(selector).all()
+                    for element in elements:
+                        if await element.is_visible():
+                            await element.evaluate("element => element.remove()")
+
+                page_source = await page.content()
+                elements = partition_html(text=page_source)
+                text = "\n\n".join([str(el) for el in elements])
+                metadata = {"source": url}
+                docs.append(Document(page_content=text, metadata=metadata))
+            except Exception as e:
+                if self.continue_on_failure:
+                    logger.error(
+                        f"Error fetching or processing {url}, exception: {e}"
+                    )
+                else:
+                    raise e
         return docs
